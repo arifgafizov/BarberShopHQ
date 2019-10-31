83081798a77def145e47c2470e625286c42f2545
Arif Gafizov <agafizov@gmail.com>
2019-10-31 12:46:33 +0300

    Add visit.erb, plugins datetimepicker and simplecolorpicker, save clients to database

--- app.rb
+++ app.rb
@@ -12,7 +12,28 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@barbers = Barber.all
+end
+
+get '/' do
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	clients = Client.create(name: @username, phone: @phone, datestamp: @datetime, barber: @barber, color: @color)
+
+	erb "<h2>Спасибо вы записались на #{@datetime}</h2>"
+
 end
